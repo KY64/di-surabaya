6b02d7784e5b153d1c3d9954a260fcdcca3629d1
KY64 <taurus99@protonmail.com>
2024-10-30 21:29:27 +0700

    feat: add contact section

--- src/Web/render.js
+++ src/Web/render.js
@@ -3,9 +3,12 @@ import fs from "node:fs";
 
 const { BUS_MAP_FILE_ID, TRAIN_MAP_FILE_ID } = process.env;
 
+const data = JSON.parse(fs.readFileSync("data.json"));
+
 const values = {
   BUS_MAP_FILE_ID,
   TRAIN_MAP_FILE_ID,
+  data
 };
 
 const result = nunjucks.render("index.njk", values);
